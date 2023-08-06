ddd27c955d9eb4a8771253f3fedba8c897adf76e
mehedidev <mdmehedi23234@gmail.com>
2023-08-06 16:37:18 +0600

    first commit

---
+++ pages/[id].jsx
@@ -0,0 +1,12 @@
+
+import Profile from '@/src/component/app/Profile/Profile'
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+<>
+<Profile/>
+</>
+  )
+}
